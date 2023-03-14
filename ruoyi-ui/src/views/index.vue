8afbab3a39767dff5ef97d3dcafb4dde3226525d
JavaSkx <skx1343501229@163.com>
2023-03-14 12:44:21 +0800

    [FIX]目录栏

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -2,70 +2,70 @@
   <div class="app-container home">
     <el-row :gutter="20">
       <el-col :sm="24" :lg="24">
-        <blockquote class="text-warning" style="font-size: 14px">
-          领取阿里云通用云产品1888优惠券
-          <br />
-          <el-link
-            href="https://www.aliyun.com/minisite/goods?userCode=brki8iof"
-            type="primary"
-            target="_blank"
-            >https://www.aliyun.com/minisite/goods?userCode=brki8iof</el-link
-          >
-          <br />
-          领取腾讯云通用云产品2860优惠券
-          <br />
-          <el-link
-            href="https://cloud.tencent.com/redirect.php?redirect=1025&cps_key=198c8df2ed259157187173bc7f4f32fd&from=console"
-            type="primary"
-            target="_blank"
-            >https://cloud.tencent.com/redirect.php?redirect=1025&cps_key=198c8df2ed259157187173bc7f4f32fd&from=console</el-link
-          >
-          <br />
-          阿里云服务器折扣区
-          <el-link href="http://aly.ruoyi.vip" type="primary" target="_blank"
-            >>☛☛点我进入☚☚</el-link
-          >
-          &nbsp;&nbsp;&nbsp; 腾讯云服务器秒杀区
-          <el-link href="http://txy.ruoyi.vip" type="primary" target="_blank"
-            >>☛☛点我进入☚☚</el-link
-          ><br />
-          <h4 class="text-danger">
-            云产品通用红包，可叠加官网常规优惠使用。(仅限新用户)
-          </h4>
-        </blockquote>
+<!--        <blockquote class="text-warning" style="font-size: 14px">-->
+<!--          领取阿里云通用云产品1888优惠券-->
+<!--          <br />-->
+<!--          <el-link-->
+<!--            href="https://www.aliyun.com/minisite/goods?userCode=brki8iof"-->
+<!--            type="primary"-->
+<!--            target="_blank"-->
+<!--            >https://www.aliyun.com/minisite/goods?userCode=brki8iof</el-link-->
+<!--          >-->
+<!--          <br />-->
+<!--          领取腾讯云通用云产品2860优惠券-->
+<!--          <br />-->
+<!--          <el-link-->
+<!--            href="https://cloud.tencent.com/redirect.php?redirect=1025&cps_key=198c8df2ed259157187173bc7f4f32fd&from=console"-->
+<!--            type="primary"-->
+<!--            target="_blank"-->
+<!--            >https://cloud.tencent.com/redirect.php?redirect=1025&cps_key=198c8df2ed259157187173bc7f4f32fd&from=console</el-link-->
+<!--          >-->
+<!--          <br />-->
+<!--          阿里云服务器折扣区-->
+<!--          <el-link href="http://aly.ruoyi.vip" type="primary" target="_blank"-->
+<!--            >>☛☛点我进入☚☚</el-link-->
+<!--          >-->
+<!--          &nbsp;&nbsp;&nbsp; 腾讯云服务器秒杀区-->
+<!--          <el-link href="http://txy.ruoyi.vip" type="primary" target="_blank"-->
+<!--            >>☛☛点我进入☚☚</el-link-->
+<!--          ><br />-->
+<!--          <h4 class="text-danger">-->
+<!--            云产品通用红包，可叠加官网常规优惠使用。(仅限新用户)-->
+<!--          </h4>-->
+<!--        </blockquote>-->
 
-        <hr />
+<!--        <hr />-->
       </el-col>
     </el-row>
     <el-row :gutter="20">
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>若依后台管理框架</h2>
-        <p>
-          一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了若依管理系统，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。
-        </p>
-        <p>
-          <b>当前版本:</b> <span>v{{ version }}</span>
-        </p>
-        <p>
-          <el-tag type="danger">&yen;免费开源</el-tag>
-        </p>
-        <p>
-          <el-button
-            type="primary"
-            size="mini"
-            icon="el-icon-cloudy"
-            plain
-            @click="goTarget('https://gitee.com/y_project/RuoYi-Vue')"
-            >访问码云</el-button
-          >
-          <el-button
-            size="mini"
-            icon="el-icon-s-home"
-            plain
-            @click="goTarget('http://ruoyi.vip')"
-            >访问主页</el-button
-          >
-        </p>
+<!--        <h2>若依后台管理框架</h2>-->
+<!--        <p>-->
+<!--          一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了若依管理系统，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。-->
+<!--        </p>-->
+<!--        <p>-->
+<!--          <b>当前版本:</b> <span>v{{ version }}</span>-->
+<!--        </p>-->
+<!--        <p>-->
+<!--          <el-tag type="danger">&yen;免费开源</el-tag>-->
+<!--        </p>-->
+<!--        <p>-->
+<!--          <el-button-->
+<!--            type="primary"-->
+<!--            size="mini"-->
+<!--            icon="el-icon-cloudy"-->
+<!--            plain-->
+<!--            @click="goTarget('https://gitee.com/y_project/RuoYi-Vue')"-->
+<!--            >访问码云</el-button-->
+<!--          >-->
+<!--          <el-button-->
+<!--            size="mini"-->
+<!--            icon="el-icon-s-home"-->
+<!--            plain-->
+<!--            @click="goTarget('http://ruoyi.vip')"-->
+<!--            >访问主页</el-button-->
+<!--          >-->
+<!--        </p>-->
       </el-col>
 
       <el-col :sm="24" :lg="12" style="padding-left: 50px">
@@ -120,7 +120,7 @@
             <p>
               <i class="el-icon-user-solid"></i> QQ群：<s>满937441</s> <s>满887144332</s>
               <s>满180251782</s> <s>满104180207</s> <s>满186866453</s> <s>满201396349</s>
-              <s>满101456076</s> <s>满101539465</s> <s>满264312783</s> <s>满167385320</s> 
+              <s>满101456076</s> <s>满101539465</s> <s>满264312783</s> <s>满167385320</s>
               <s>满104748341</s> <s>满160110482</s> <s>满170801498</s> <a href="https://jq.qq.com/?_wv=1027&k=eCx8eyoJ" target="_blank">
                 108482800</a
               >
@@ -882,23 +882,23 @@
           </el-collapse>
         </el-card>
       </el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="8">
-        <el-card class="update-log">
-          <div slot="header" class="clearfix">
-            <span>捐赠支持</span>
-          </div>
-          <div class="body">
-            <img
-              src="https://oscimg.oschina.net/oscnet/up-d6695f82666e5018f715c41cb7ee60d3b73.png"
-              alt="donate"
-              width="100%"
-            />
-            <span style="display: inline-block; height: 30px; line-height: 30px"
-              >你可以请作者喝杯咖啡表示鼓励</span
-            >
-          </div>
-        </el-card>
-      </el-col>
+<!--      <el-col :xs="24" :sm="24" :md="12" :lg="8">-->
+<!--        <el-card class="update-log">-->
+<!--          <div slot="header" class="clearfix">-->
+<!--            <span>捐赠支持</span>-->
+<!--          </div>-->
+<!--          <div class="body">-->
+<!--            <img-->
+<!--              src="https://oscimg.oschina.net/oscnet/up-d6695f82666e5018f715c41cb7ee60d3b73.png"-->
+<!--              alt="donate"-->
+<!--              width="100%"-->
+<!--            />-->
+<!--            <span style="display: inline-block; height: 30px; line-height: 30px"-->
+<!--              >你可以请作者喝杯咖啡表示鼓励</span-->
+<!--            >-->
+<!--          </div>-->
+<!--        </el-card>-->
+<!--      </el-col>-->
     </el-row>
   </div>
 </template>
